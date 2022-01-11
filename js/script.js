833e2adb0df8ab50d6f04690d86bc38bd3b84719
Mateusz Grden <matgrden@gmail.com>
2022-01-11 22:11:43 +0100

    table and function render added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 {
-  const init = () => {
+  const tasks = [
+    {
+      content: "zjeść kolację",
+      done: true,
+    },
+    {
+      content: "iść spać",
+      done: false,
+    },
+  ];
 
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+        <li>
+          ${task.content}
+        </li>
+        `;
+    }
+    document.querySelector(".js-listTasks").innerHTML = htmlString;
+  };
+
+  const init = () => {
+    render();
   };
 
   init();
